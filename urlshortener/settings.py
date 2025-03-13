6b0010088f6b9c209a466e1cc871e31a6d497916
Aakash Mahajan <mahajanaakash108@gmail.com>
2025-03-13 16:48:34 +0530

    feat: CORS policy updated

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'url.apps.UrlConfig',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # Allow all origins
 
 ROOT_URLCONF = 'urlshortener.urls'
 
